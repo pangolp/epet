2032e42681d8a481a97dd62690ba6e6ee7db92e2
Pagani Walter <paganiwalter@gmail.com>
2019-04-23 17:21:49 -0300

    Agregue la opción de ampliar las novedades
    Se agrega HTML, se permite ingresando en el boton leer mas,
    acceder a cada una de las novedades.

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WebIndex
+from .views import WebIndex, NovedadDetalle
 
 
 app_name = 'web'
 urlpatterns = [
 	path('', WebIndex.as_view(), name='index'),
+	path('novedad/<int:pk>/', NovedadDetalle.as_view(), name='novedad_id'),
 ]
--- apps/web/views.py
+++ apps/web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+from django.views.generic.detail import DetailView
 from .models import Novedad
 
 
@@ -7,3 +8,9 @@ class WebIndex(ListView):
 	queryset = Novedad.objects.order_by('-fecha_publicación')[:5]
 	template_name = 'web/index.html'
 	context_object_name = 'novedades'
+
+
+class NovedadDetalle(DetailView):
+	model = Novedad
+	template_name = 'web/novedad/novedaddetalle.html'
+	context_object_name = 'novedad'
